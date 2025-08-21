59bac8a18a198df1b37de96f12010a9ce27b9d3f
Barbara Cunha <barbara.torres.cunha@gmail.com>
2025-08-21 18:00:11 -0300

    "Adicionada lógica de pontuação"

--- javascript-palpite.js
+++ javascript-palpite.js
@@ -1,52 +1,39 @@
-var round = 0;
+// Listas
+const fotos = {"fotos/exemplo-corgi.png": 2019, "fotos/exemplo-pinguim.jpg": 2020, "fotos/exemplo-lontra.png": 2021};
+const acertos = [];
+
+// Variáveis
+var round = 1;
 var ano_escolhido;
-console.log("Pontuação: " + pontuacao(5,10));
-console.log("Pontuação: " + pontuacao(10,10));
-console.log("Pontuação: " + pontuacao(9,10));
-console.log("Pontuação: " + pontuacao(10,8));
-console.log("Pontuação: " + pontuacao(10,7));
+var pontuacaoFinal = 0;
 
-// MAIN
-
-newRound();
+// CONFIGURAÇÃO INICIAL
+document.getElementById("1").style.color = "#fff";
+document.getElementById("1").style.backgroundColor = "#8448d7"
 
 /* Função para exibir ano selecionado */
-const input_ano = document.querySelector("input");
-input_ano.addEventListener("change", () => {
-    document.getElementById("ano-selecionado").innerHTML = input_ano.value;
+const inputAno = document.querySelector("input");
+inputAno.addEventListener("change", () => {
+    document.getElementById("ano-selecionado").innerHTML = inputAno.value;
 })
 
 
 /* Função do Aperto do Botão Enviar */
 function newRound(){
-    // Zera mostrador do ano
-    document.getElementById("ano-selecionado").innerHTML = "----";
+    // Captura os pontos da rodada
+    let fotoTela = document.getElementById("foto-palpite"); // foto atual
+    let anoFotoTela = fotos[fotoTela.getAttribute("src")];
+    
+    pontuacaoFinal += pontuacao(anoFotoTela, inputAno.value);
+    
+    // Exibe pontuação
+    document.getElementById("pontos-atuais").innerHTML = pontuacaoFinal + "/100"
 
-    if (round != 5) {
-        if(round != 0){
-            var ano_escolhido = document.getElementById(input-ano).value;
-            console.log("Ano Escolhido: " + ano_escolhido);
 
-            // Retorna visual do elemento atual
-            var elemento_round = document.getElementById(round.toString());
-            elemento_round.style.backgroundColor = "#fff";
-            elemento_round.style.color = "#8448d7";
-        }
+    // Muda mostrador
+    mudaMostradorRound();
 
-        // Muda o round e visual do elemento
-        round++;
-        elemento_round = document.getElementById(round.toString());
-        //console.log(elemento_round);
-        elemento_round.style.backgroundColor = "#8448d7";
-        elemento_round.style.color = "#fff";
-
-        // Escolhe a foto
-
-    } else {
-
-        round = 1;
-        console.log("Próxima Tela");
-    }
+    
     
 }
 
@@ -63,9 +50,43 @@ function pontuacao(anoEscolhido, anoCorreto){
 
     // Retorno de Pontos
     switch (diferenca){
-        case 0: return 20;
-        case 1: return 10;
-        case 2: return 5;
-        default: return 0;
+        case 0: acertos.push(1); return 20;
+        case 1: acertos.push(0); return 10;
+        case 2: acertos.push(0); return 5;
+        default: acertos.push(0); return 0;
     }
 }
+
+function mudaMostradorRound(){
+// Zera mostrador do ano
+    document.getElementById("ano-selecionado").innerHTML = "----";
+
+    if (round != 5) {
+    
+        var ano_escolhido = inputAno.value;
+        console.log("Ano Escolhido: " + ano_escolhido);
+
+        // Retorna visual do elemento atual
+        var elemento_round = document.getElementById(round.toString());
+        elemento_round.style.backgroundColor = "#fff";
+        elemento_round.style.color = "#8448d7";
+        
+
+        // Muda o round e visual do elemento
+        round++;
+        elemento_round = document.getElementById(round.toString());
+        //console.log(elemento_round);
+        elemento_round.style.backgroundColor = "#8448d7";
+        elemento_round.style.color = "#fff";
+
+        // Escolhe a foto
+
+    } else {
+
+        round = 1;
+        console.log("Próxima Tela");
+        console.log("Pontuação Final: " + pontuacaoFinal);
+        console.log("Acertos: " + acertos);
+    }
+}
+
