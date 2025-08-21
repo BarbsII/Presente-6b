a294ca387fbf88a330e6c22c5f1a8f46b329bc0e
Barbara Cunha <barbara.torres.cunha@gmail.com>
2025-08-21 16:03:07 -0300

    Adicionada mudança visual dos rounds

---
+++ javascript-palpite.js
@@ -0,0 +1,32 @@
+// MAIN
+var round = 0;
+
+newRound();
+
+/* Função para mudar aparência dos Rounds */
+function newRound(){
+
+    if (round != 5) {
+        if(round != 0){
+            // Retorna visual do elemento atual
+            var elemento_round = document.getElementById(round.toString());
+            elemento_round.style.backgroundColor = "#fff";
+            elemento_round.style.color = "#8448d7";
+        }
+
+        // Muda o round e visual do elemento
+        round++;
+        elemento_round = document.getElementById(round.toString());
+        //console.log(elemento_round);
+        elemento_round.style.backgroundColor = "#8448d7";
+        elemento_round.style.color = "#fff";
+
+        // Escolhe a foto
+
+    } else {
+
+        round = 1;
+        console.log("Próxima Tela");
+    }
+    
+}
